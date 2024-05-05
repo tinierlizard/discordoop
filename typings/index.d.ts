5dce197ae4ead13c9917cab28e57fb50d9f0f8a1
tinierlizard <tinierlizard145@gmail.com>
2024-05-05 02:21:54 -0600

    fixed event class definition, eventdata struct

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,37 +1,41 @@
-import { ApplicationCommandOptionData, Client, CommandInteraction, Events } from "discord.js";
+import { ApplicationCommandOptionData, Client, ClientEvents, CommandInteraction } from "discord.js";
 
+// Classes
 export class Event {
     constructor(data: EventData);
     data: EventData;
     public init(client: Client): Promise<boolean>;
-    public run(): StatusMessage;
+    public run(any): void;
 }
 
 export class Command  {
     constructor(data: CommandData);
     data: CommandData;
-    public run(int: CommandInteraction): StatusMessage;
+    public run(int: CommandInteraction): Promise<StatusMessage>;
     public init?(int: CommandInteraction): null;
 }
 
+// Interfaces
 export interface StatusMessage {
     code: string;
     message?: string;
 }
 
+// Data Structures
 export interface CommandData {
     name: string;
     isNSFW: boolean;
     runContext: "USER" | "MSG" | "CHI";
     desc: string;
     dmPermission?: boolean | undefined;
-    run?(int: CommandInteraction): StatusMessage;
+    run?(int: CommandInteraction): Promise<StatusMessage>;
     init?(int: CommandInteraction): null;
     options?: Array<ApplicationCommandOptionData>;
 }
 
 export interface EventData {
-    type: Events;
+    type: keyof ClientEvents;
+    client?;
     init?(client: Client): Promise<boolean>;
-    run?(): StatusMessage;
+    run?(any): void;
 }
